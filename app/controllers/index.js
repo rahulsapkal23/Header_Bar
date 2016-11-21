69ae3daa26396739f1118f40d834571d1aac2c63
webwerks1 <webwerks1@Rahul-Mac-mini.local>
2016-11-21 16:14:45 +0530

    befor clone

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,24 +1,14 @@
-/*var win = Ti.UI.iOS.createNavigationWindow({
+var win = Ti.UI.iOS.createNavigationWindow({
     window: Ti.UI.createWindow({
         title: "Modal Window"
     })
 
 
-    })
+
 });
 
 win.open({
     modal: true,
     modalTransitionStyle: Ti.UI.iOS.MODAL_TRANSITION_STYLE_FLIP_HORIZONTAL,
     modalStyle: Ti.UI.iOS.MODAL_PRESENTATION_FORMSHEET
-});*/
-var currentdate = new Date()
-var date = new Date();
-$.date.text = "currentdate :" + currentdate;
-date.setMinutes(date.getMinutes() + 20);
-var diiference = date.getMinutes() - new Date().getMinutes();
-
-$.min.text = "add 20 :" + date;
-$.min20.text = "diiference :" + diiference;
-$.sec.text = "sec" + $.min20.text;
-$.win.open();
+});
